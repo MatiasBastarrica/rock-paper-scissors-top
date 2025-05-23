9383842961aca492100ad9ec77f1c217008bd795
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2025-05-23 12:47:43 -0300

    Write the logic to get the computer choice
    A function called getComputerChoice() will return
    the choice of the computer. In order to create
    this choice I wrote another function inside it named
    getOneTwoOrThree which will generate an random
    number between 1 and 3 (inclusive) and assign each
    number to a string of "rock", "paper" or "scissors"
    respectively.

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Hello World!");
+// ### PSEUDOCOE ###
+// ## LOGIC TO GET THE COMPUTER CHOICE ##
+
+function getComputerChoice() {
+  function GetOneTwoOrThree() {
+    return Math.floor(Math.random() * (2 + 1) + 1);
+  }
+  let oneTwoThree = GetOneTwoOrThree();
+  switch (oneTwoThree) {
+    case 1:
+      console.log("Computer: rock");
+      return "rock";
+      break;
+    case 2:
+      console.log("Computer: paper");
+      return "paper";
+      break;
+    case 3:
+      console.log("Computer: scissors");
+      return "scissors";
+      break;
+
+    default:
+      break;
+  }
+}
+
+// getComputerChoice();
